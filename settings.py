32a14b57f3b8e4280ddc6a5d6142a0ceae56a3b5
Alister Cordiner <alister@cordiner.net>
2015-05-21 00:22:56 +1000

    Travis-CI setup

---
+++ settings.py
@@ -0,0 +1,16 @@
+SECRET_KEY = 'uak)i#z(gsj(89z#_^9pk*@3evxtbxsccx1$u#50de3n7thuqm'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    },
+}
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'emailusername',
+)
+
+AUTH_USER_MODEL = 'emailusername.User'
