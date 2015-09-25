66fa989f8ba61afcfa5c4b00a49716ab59988967
Alister Cordiner <alister@cordiner.net>
2015-09-25 20:25:39 +1000

    Bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ PROJECT_DIR = os.path.dirname(__file__)
 
 setup(
     name='django-email-username',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
     url='http://github.com/acordiner/django-email-username',
     license='GPL v2',
